752dbb75b3d3d03defeab290f1caf31e94c59583
Kendry Grullon <kengrullon@gmail.com>
2023-03-09 17:19:26 -0400

    Finished up functions return.

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
     another_function(5);
     print_labeled_measurement(5, 'h');
+    let x = five();
+    println!("The returned function value: {x}");
+    let y = plus_one(x);
+    println!("The returned plus one: {y}");
 }
 
 fn another_function(x: i32) {
@@ -9,4 +13,12 @@ fn another_function(x: i32) {
 
 fn print_labeled_measurement(x: i32, unit_label: char) {
     println!("The measurement is: {x}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
