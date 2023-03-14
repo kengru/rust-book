9288cf1e356626d6f3896a0d97b5a94a098a1d44
Kendry Grullon <kengrullon@gmail.com>
2023-03-14 11:55:59 -0400

    Finished borrowing.

--- borrowing/src/main.rs
+++ borrowing/src/main.rs
@@ -20,8 +20,35 @@ fn main() {
     let s_len1 = str::len(&s);
     let s_len2 = s.len();
     assert_eq!(s_len1, s_len2);
+
+    vec_mut();
+
+    // Checking permissions.
+    let mut v = vec!['h', 'e', 'l', 'l', 'o'];
+    let vv = &mut v;
+    ascii_capitalize(vv);
 }
 
 fn greet(g1: &String, g2: &String) {
     println!("{} {}!", g1, g2);
+}
+
+fn vec_mut() {
+    let mut vect = vec![1, 2, 3];
+    let num = &mut vect[2];
+
+    *num += 1;
+    println!("Third element is {}", *num);
+
+    println!("Vector is now {:?}", vect);
+}
+
+fn ascii_capitalize(v: &mut Vec<char>) {
+    let c = &v[0];
+    if c.is_ascii_lowercase() {
+        let up = c.to_ascii_uppercase();
+        v[0] = up;
+    } else {
+        println!("Already capitalized: {:?}", v);
+    }
 }
